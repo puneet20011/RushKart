9e5d206fc3b9721addad30fe649998c742af5d4d
Puneet Singh Puri <57475429+puneet20011@users.noreply.github.com>
2025-08-04 17:51:58 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <h1>Welcome to RushKart</h1>
-  )
-}
+  return <h1>Welcome to RushKart</h1>;
+};
 
-export default App
+export default App;
