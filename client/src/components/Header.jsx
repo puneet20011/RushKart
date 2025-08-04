ea69ce00191c08f1309b0a12b59faf7d3c7b1a49
Puneet Singh Puri <57475429+puneet20011@users.noreply.github.com>
2025-08-04 19:34:04 +0530

    commit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,21 +1,28 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
   return (
     <header>
       <Navbar className="navbar-orange" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">RushKart</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>RushKart</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
+                <LinkContainer to='/cart'>
+              <Nav.Link>
                 <FaShoppingCart /> Cart
               </Nav.Link>
-              <Nav.Link href="/login">
+              </LinkContainer>
+              <LinkContainer to='/login' >
+              <Nav.Link>
                 <FaUser /> Sign In
               </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
