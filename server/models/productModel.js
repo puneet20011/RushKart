edba1548c97927e9a8f5e60b528b9ccc6b0240ac
Puneet Singh Puri <57475429+puneet20011@users.noreply.github.com>
2025-08-05 18:27:27 +0530

    commit

---
+++ server/models/productModel.js
@@ -0,0 +1,81 @@
+import mongoos from "mongoose";
+
+const reviewSchema = new mongoos.Schema(
+  {
+    user: {
+      type: mongoos.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const productSchema = new mongoos.Schema(
+  {
+    user: {
+      type: mongoos.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoos.model("Product", productSchema);
+
+export default Product;
