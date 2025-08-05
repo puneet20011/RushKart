08c40f8679b5503ee32dd6b83850ebf904aad29a
Puneet Singh Puri <57475429+puneet20011@users.noreply.github.com>
2025-08-05 16:19:33 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 const port = process.env.PORT || 5000;
 
-const app = express();
+connectDB(); // Connect to MongoDB
 
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("API is running...");
@@ -16,8 +18,8 @@ app.get("/api/products", (req, res) => {
 });
 
 app.get("/api/products/:id", (req, res) => {
-     const product = products.find((p) => p._id === req.params.id);
-     res.json(product);
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
 });
 
 app.listen(port, () => {
