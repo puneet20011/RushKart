acbbe3105aeb5ab535cf12d2dc74702ef4795be3
Puneet Singh Puri <57475429+puneet20011@users.noreply.github.com>
2025-08-06 20:02:40 +0530

    commit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,27 +1,39 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Badge, Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <header>
       <Navbar className="navbar-red" expand="md" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
-            <Navbar.Brand href="/" className="rushkart-logo">RushKart</Navbar.Brand>
+            <Navbar.Brand href="/" className="rushkart-logo">
+              RushKart
+            </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-                <LinkContainer to='/cart'>
-              <Nav.Link>
-                <FaShoppingCart /> Cart
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                  {
+                    cartItems.length > 0 && (
+                     <Badge pill bg="warning" style={{marginLeft: "5px"}}>
+                      { cartItems.reduce((a, c) => a + c.qty, 0 )}
+                     </Badge>
+                    )
+                  }
+                </Nav.Link>
               </LinkContainer>
-              <LinkContainer to='/login' >
-              <Nav.Link>
-                <FaUser /> Sign In
-              </Nav.Link>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser /> Sign In
+                </Nav.Link>
               </LinkContainer>
             </Nav>
           </Navbar.Collapse>
