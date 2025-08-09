25f836ac37444d7b8eae33b82bda54aa33a5a267
Puneet Singh Puri <57475429+puneet20011@users.noreply.github.com>
2025-08-09 13:36:05 +0530

    commit

---
+++ client/src/screens/PaymentScreen.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import {Form, Button, Col} from "react-bootstrap";
+import FormContainer from "../components/FormContainer"
+import CheckoutSteps from "../components/checkoutSteps";
+
+const PaymentScreen = () => {
+  return (
+    <div>PaymentScreen</div>
+  )
+}
+
+export default PaymentScreen
