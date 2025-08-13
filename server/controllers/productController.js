3e1777b943b8281147081fea886ceab3e352b820
Puneet Singh Puri <57475429+puneet20011@users.noreply.github.com>
2025-08-13 19:51:42 +0530

    commit

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,7 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import Product from "../models/productModel.js";
 
-
 // @desc Fetch all products
 // @route GET /api/products
 // @access Public
@@ -9,7 +8,6 @@ const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
   res.json(products);
 });
-
 
 // @desc Fetch a product
 // @route GET /api/products/:id
@@ -25,4 +23,24 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById };
+// @desc Create a product
+// @route POST /api/products
+// @access Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+export { getProducts, getProductById, createProduct };
